3c72c6a82f1c653b4b480b9b8bdd80cb781bd2d1
ahmedmanubariya <ahmed-m2011@hotmail.co.uk>
2026-08-25 11:43:33 +0100

    feat: register new overview, controlled documents and system modules

--- traininghub/__init__.py
+++ traininghub/__init__.py
@@ -6,10 +6,12 @@ from .account_admin import bp as account_admin_bp
 from .alerts import bp as alerts_bp
 from .bootstrap import ensure_admin
 from .db import init_app
+from .experience import bp as experience_bp
 from .hooks import bp as hooks_bp
 from .routes import bp
 from .sop_admin import bp as sop_admin_bp
 from .staff_admin import bp as staff_admin_bp
+from .system_admin import bp as system_admin_bp
 
 
 def create_app(test_config=None):
@@ -19,6 +21,7 @@ def create_app(test_config=None):
         DATABASE=os.environ.get('DATABASE', 'traininghub.db'),
         UPLOAD_FOLDER=os.environ.get('UPLOAD_FOLDER', 'uploads'),
         ALERT_THRESHOLD=int(os.environ.get('ALERT_THRESHOLD', '80')),
+        APPROVED_DOCS_ROOT=os.environ.get('APPROVED_DOCS_ROOT', ''),
     )
     if test_config:
         app.config.update(test_config)
@@ -27,9 +30,11 @@ def create_app(test_config=None):
     with app.app_context():
         ensure_admin()
     app.register_blueprint(bp)
+    app.register_blueprint(experience_bp)
     app.register_blueprint(alerts_bp)
     app.register_blueprint(account_admin_bp)
     app.register_blueprint(sop_admin_bp)
     app.register_blueprint(staff_admin_bp)
+    app.register_blueprint(system_admin_bp)
     app.register_blueprint(hooks_bp)
     return app
